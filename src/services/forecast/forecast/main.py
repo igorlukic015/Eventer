3374c808c528cb19c873e27f593f46ff1dcb0148
cigor99 <igi.l.1999@gmail.com>
2023-06-03 21:52:06 +0200

    Add reading arguments and seeding db

--- src/services/forecast/forecast/main.py
+++ src/services/forecast/forecast/main.py
@@ -1,17 +1,41 @@
 from uvicorn import run as uvicorn_run
 from fastapi import FastAPI
 from forecast.config import config
+from forecast.service import seed_city_cache
 from router import router
+from sys import argv, exit
+from getopt import getopt, GetoptError
 
 app = FastAPI()
 
 app.include_router(router)
 
 
-if __name__ == "__main__":
+def main(arguments):
+    try:
+        opts, args = getopt(arguments, "", ["seed="])
+
+    except GetoptError:
+        print("Error parsing arguments")
+        exit(1)
+
+    should_seed = False
+
+    for opt, arg in opts:
+        if opt == "--seed":
+            should_seed = bool(arg)
+
+    if should_seed:
+        seed_city_cache()
+        exit(0)
+
     uvicorn_run("main:app",
                 host=config.host,
                 port=config.port,
                 reload=config.shouldReload,
                 log_level=config.log_level,
                 debug=config.is_debug)
+
+
+if __name__ == "__main__":
+    main(argv[1:])

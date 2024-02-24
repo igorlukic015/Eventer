413d7ab595d976dd37e629e2c39cadd6a2b05ff8
cigor99 <igi.l.1999@gmail.com>
2024-02-24 13:36:26 +0100

    Add image configuration

---
+++ src/services/user/src/main/java/com/eventer/user/config/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package com.eventer.user.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+@Profile(value = "dev")
+@Component
+public class ApplicationConfiguration {
+    private static String imageBaseUrl;
+
+    public static String getImageBaseUrl() {
+        return imageBaseUrl;
+    }
+
+    public static String getImageUrlSeparator() {
+        return FileSystems.getDefault().getSeparator();
+    }
+
+    @Value("${image.base.url}")
+    public void setImageBaseUrl(String imageBaseUrl) {
+        Path absolutePath = Path.of(imageBaseUrl).toAbsolutePath();
+        ApplicationConfiguration.imageBaseUrl = absolutePath.toString().replace(File.pathSeparator, "/");
+    }
+}

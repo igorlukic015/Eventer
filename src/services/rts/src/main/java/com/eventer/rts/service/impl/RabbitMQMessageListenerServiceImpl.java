1707fd0847ddd62a7bfbe4cc0a55fce87d7dc915
cigor99 <igi.l.1999@gmail.com>
2024-02-28 14:34:55 +0100

    Add receiving messages functionality to rts

--- src/services/rts/src/main/java/com/eventer/rts/service/impl/RabbitMQMessageListenerServiceImpl.java
+++ src/services/rts/src/main/java/com/eventer/rts/service/impl/RabbitMQMessageListenerServiceImpl.java
@@ -1,24 +1,58 @@
 package com.eventer.rts.service.impl;
 
 import com.eventer.rts.contracts.ApplicationStatics;
+import com.eventer.rts.contracts.Message;
 import com.eventer.rts.service.MessageListenerService;
-import com.eventer.rts.web.v1.SSEController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class RabbitMQMessageListenerServiceImpl implements MessageListenerService {
-    private final SSEController sseController;
+//    private final ObjectMapper objectMapper;
 
-    public RabbitMQMessageListenerServiceImpl(SSEController sseController) {
-        this.sseController = sseController;
-    }
+    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+//    public RabbitMQMessageListenerServiceImpl(ObjectMapper objectMapper) {
+//        this.objectMapper = objectMapper;
+//    }
 
     @RabbitListener(queues = ApplicationStatics.EVENTER_DATA_MESSAGE_QUEUE)
     @Override
     public void receiveMessage(String message) {
-        System.out.println("Received: " + message);
+//        Message parsedMessage;
+//        try {
+//            parsedMessage = this.objectMapper.readValue(message, Message.class);
+//
+//        } catch (IllegalArgumentException | JsonProcessingException e) {
+//            // TODO: All logging
+//            return;
+//        }
 
-        this.sseController.pushToClient(message);
+//        System.out.println("Received: " + parsedMessage.getName());
+//
+//        System.out.println(parsedMessage.getData());
+
+        this.pushToClient(message);
+    }
+
+    @Override
+    public CopyOnWriteArrayList<SseEmitter> getEmitters() {
+        return emitters;
+    }
+
+    private void pushToClient(String message) {
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(SseEmitter.event().name("message").data(message));
+            } catch (Exception e) {
+                emitters.remove(emitter);
+            }
+        }
     }
 }
--- src/services/rts/src/main/java/com/eventer/rts/web/v1/SSEController.java
+++ src/services/rts/src/main/java/com/eventer/rts/web/v1/SSEController.java
@@ -1,32 +1,29 @@
 package com.eventer.rts.web.v1;
 
+import com.eventer.rts.service.MessageListenerService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 public class SSEController {
-    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    private final MessageListenerService messageListenerService;
+
+    public SSEController(MessageListenerService messageListenerService) {
+        this.messageListenerService = messageListenerService;
+    }
 
     @GetMapping("/stream")
     public SseEmitter stream() {
         SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-        this.emitters.add(emitter);
+        this.messageListenerService.getEmitters().add(emitter);
 
-        emitter.onCompletion(() -> this.emitters.remove(emitter));
-        emitter.onTimeout(() -> this.emitters.remove(emitter));
+        emitter.onCompletion(() -> this.messageListenerService.getEmitters().remove(emitter));
+        emitter.onTimeout(() -> this.messageListenerService.getEmitters().remove(emitter));
 
         return emitter;
-    }
-
-    public void pushToClient(String message) {
-        for (SseEmitter emitter : emitters) {
-            try {
-                emitter.send(SseEmitter.event().name("message").data(message));
-            } catch (Exception e) {
-                emitters.remove(emitter);
-            }
-        }
     }
 }

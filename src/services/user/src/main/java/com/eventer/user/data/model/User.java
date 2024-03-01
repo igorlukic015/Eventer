1048bbdfc7f8c7e52f45ba870aaa7ab73307112c
cigor99 <igi.l.1999@gmail.com>
2024-03-01 14:28:39 +0100

    Fix user table name

--- src/services/user/src/main/java/com/eventer/user/data/model/User.java
+++ src/services/user/src/main/java/com/eventer/user/data/model/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "user")
+@Table(name = "eventer_user")
 public class User extends AbstractAuditingEntity<Long> {
 
     @Id

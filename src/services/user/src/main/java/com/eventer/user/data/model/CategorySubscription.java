bb1d9af3f0d5497870e07bbd169dd446888db02e
igorlukic015 <igor.lukic015@gmail.com>
2024-03-27 12:30:34 +0100

    Add subscription models

---
+++ src/services/user/src/main/java/com/eventer/user/data/model/CategorySubscription.java
@@ -0,0 +1,36 @@
+package com.eventer.user.data.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "category_subscription")
+public class CategorySubscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_subscription_seq")
+    @SequenceGenerator(name = "category_subscription_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "categoryId", nullable = false)
+    private Long categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategorySubscription that = (CategorySubscription) o;
+        return Objects.equals(id, that.id)
+                || (Objects.equals(categoryId, that.categoryId) && Objects.equals(user, that.user));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, categoryId, user.getId());
+    }
+}

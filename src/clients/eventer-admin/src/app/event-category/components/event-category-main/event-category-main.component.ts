0b012c4cd964a50e947bc16991b4c053e83ca34f
igorlukic015 <igor.lukic015@gmail.com>
2024-03-29 12:30:15 +0100

    Add basic table to categories

--- src/clients/eventer-admin/src/app/event-category/components/event-category-main/event-category-main.component.ts
+++ src/clients/eventer-admin/src/app/event-category/components/event-category-main/event-category-main.component.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import {LayoutMainComponent} from "../../../shared/layout-main/layout-main.component";
 import {FooterComponent} from "../../../shared/footer/footer.component";
 import {NavbarComponent} from "../../../shared/navbar/navbar.component";
+import {EventCategoryListComponent} from "../event-category-list/event-category-list.component";
+import {CommonModule} from "@angular/common";
+import {ActionBarComponent} from "../../../shared/action-bar/action-bar.component";
 
 @Component({
   selector: 'eventer-admin-event-category-main',
   standalone: true,
-    imports: [
-        LayoutMainComponent,
-        FooterComponent,
-        NavbarComponent
-    ],
+  imports: [
+    CommonModule,
+    LayoutMainComponent,
+    FooterComponent,
+    NavbarComponent,
+    EventCategoryListComponent,
+    ActionBarComponent
+  ],
   templateUrl: './event-category-main.component.html',
   styleUrl: './event-category-main.component.css'
 })

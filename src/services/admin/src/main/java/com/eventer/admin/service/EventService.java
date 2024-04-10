5c7ddfc1c0d8d8dc5030550c56ca3ff207cf57b0
igorlukic015 <igor.lukic015@gmail.com>
2024-04-10 11:58:33 +0200

    Fix delete

--- src/services/admin/src/main/java/com/eventer/admin/service/EventService.java
+++ src/services/admin/src/main/java/com/eventer/admin/service/EventService.java
@@ -6,7 +6,6 @@ import com.github.igorlukic015.resulter.Result;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Set;
 
 public interface EventService {
--- src/services/admin/src/main/java/com/eventer/admin/utils/ResultErrorMessages.java
+++ src/services/admin/src/main/java/com/eventer/admin/utils/ResultErrorMessages.java
@@ -8,17 +8,20 @@ public class ResultErrorMessages {
     public static final String invalidEventLocation = "INVALID_EVENT_LOCATION";
     public static final String invalidCreateEventFormData = "INVALID_CREATE_EVENT_FORM_DATA";
 
+
     // EVENT CATEGORY
     public static final String categoryAlreadyExists = "CATEGORY_ALREADY_EXISTS";
     public static final String invalidEventCategoryName = "INVALID_EVENT_CATEGORY_NAME";
     public static final String invalidEventCategoryDescription =
             "INVALID_EVENT_CATEGORY_DESCRIPTION";
     public static final String categoriesNotFound = "CATEGORIES_NOT_FOUND";
-
     public static final String categoryNotFound = "CATEGORY_NOT_FOUND";
+    public static String categoryIsUsedInEvents = "CATEGORY_IS_USED_IN_EVENTS";
+
 
     // WEATHER CONDITION
     public static final String invalidWeatherCondition = "INVALID_WEATHER_CONDITION";
+
 
     // IMAGE
     public static final String imagesNotFound = "IMAGES_NOT_FOUND";
@@ -31,6 +34,7 @@ public class ResultErrorMessages {
     public static String invalidAdminUsername = "INVALID_ADMIN_USERNAME";
     public static String invalidPassword = "INVALID_ADMIN_PASSWORD";
     public static String invalidRole = "INVALID_ROLE";
+
 
     // GENERAL
     public static String failedToSendMessage = "FAILED_TO_SEND_MESSAGE";

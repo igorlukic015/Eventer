c28ba14cbb5c63c23e23df6bbf78ad8f19a8b452
igorlukic015 <igor.lukic015@gmail.com>
2024-04-16 11:38:47 +0200

    Remove commented code from real time service

--- src/services/rts/src/main/java/com/eventer/rts/service/impl/RabbitMQMessageListenerServiceImpl.java
+++ src/services/rts/src/main/java/com/eventer/rts/service/impl/RabbitMQMessageListenerServiceImpl.java
@@ -1,11 +1,7 @@
 package com.eventer.rts.service.impl;
 
 import com.eventer.rts.contracts.ApplicationStatics;
-import com.eventer.rts.contracts.Message;
 import com.eventer.rts.service.MessageListenerService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -14,30 +10,11 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class RabbitMQMessageListenerServiceImpl implements MessageListenerService {
-//    private final ObjectMapper objectMapper;
-
     private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
-
-//    public RabbitMQMessageListenerServiceImpl(ObjectMapper objectMapper) {
-//        this.objectMapper = objectMapper;
-//    }
 
     @RabbitListener(queues = ApplicationStatics.EVENTER_DATA_MESSAGE_QUEUE)
     @Override
     public void receiveMessage(String message) {
-//        Message parsedMessage;
-//        try {
-//            parsedMessage = this.objectMapper.readValue(message, Message.class);
-//
-//        } catch (IllegalArgumentException | JsonProcessingException e) {
-//            // TODO: All logging
-//            return;
-//        }
-
-//        System.out.println("Received: " + parsedMessage.getName());
-//
-//        System.out.println(parsedMessage.getData());
-
         this.pushToClient(message);
     }
 

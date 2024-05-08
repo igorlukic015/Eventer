c6fdd78422400c3551e1922753ff2075c0990c29
igorlukic015 <igor.lukic015@gmail.com>
2024-05-08 18:10:07 +0200

    Create search-list layout mockup

--- src/clients/eventer/src/app/search/components/search-main/search-main.component.ts
+++ src/clients/eventer/src/app/search/components/search-main/search-main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NavBarComponent} from "../../../shared/components/nav-bar/nav-bar.component";
+import {SearchListComponent} from "../search-list/search-list.component";
 
 @Component({
   selector: 'eventer-search-main',
   standalone: true,
   imports: [
-    NavBarComponent
+    NavBarComponent,
+    SearchListComponent
   ],
   templateUrl: './search-main.component.html',
   styleUrl: './search-main.component.css'

15dbb7a68890718852c5a14e7cdc188a33a4939d
igorlukic015 <igor.lukic015@gmail.com>
2024-05-08 23:42:36 +0200

    Add search list item

---
+++ src/clients/eventer/src/app/search/components/search-list-item/search-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eventer-search-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-list-item.component.html',
+  styleUrl: './search-list-item.component.css'
+})
+export class SearchListItemComponent {
+
+}
--- src/clients/eventer/src/app/search/components/search-list/search-list.component.ts
+++ src/clients/eventer/src/app/search/components/search-list/search-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SearchListItemComponent} from "../search-list-item/search-list-item.component";
 
 @Component({
   selector: 'eventer-search-list',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchListItemComponent
+  ],
   templateUrl: './search-list.component.html',
   styleUrl: './search-list.component.css'
 })

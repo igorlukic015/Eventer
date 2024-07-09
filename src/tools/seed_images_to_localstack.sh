d1f4f478487057db417b30c5138054938906a64d
igorlukic015 <igor.lukic015@gmail.com>
2024-07-09 12:25:32 +0200

    Refactor seed images script

--- src/tools/seed_images_to_localstack.sh
+++ src/tools/seed_images_to_localstack.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 BUCKET_NAME=eventer-images
-IMAGE_FOLDER=./seed_images/
+IMAGE_FOLDER=../services/admin/src/main/resources/seed_images/
+
+awslocal s3api create-bucket --bucket $BUCKET_NAME --region us-east-1
 
 for image in $IMAGE_FOLDER/*; do
     if [[ -f $image ]]; then
